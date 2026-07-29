3b0a6270217dfb02706fad063cd0ff07fb39823a
VitalStarRex <amehchristianc@gmail.com>
2026-07-29 19:09:07 +0100

    Update home.js

--- home.js
+++ home.js
@@ -1,43 +1,209 @@
-import { db } from "./firebase.js";
+import {db} from "./firebase.js";
+
 
 import {
+
 collection,
 query,
 orderBy,
-onSnapshot
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+onSnapshot,
+doc,
+updateDoc,
+increment
+
+}
+
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 
-const postsDiv = document.getElementById("posts");
+
+const feed = document.getElementById("feed");
+
 
 
 const q = query(
- collection(db,"posts"),
- orderBy("createdAt","desc")
+
+collection(db,"posts"),
+
+orderBy("createdAt","desc")
+
 );
+
 
 
 onSnapshot(q,(snapshot)=>{
 
-postsDiv.innerHTML="";
+
+feed.innerHTML="";
 
 
-snapshot.forEach((doc)=>{
-
-const post = doc.data();
+snapshot.forEach((item)=>{
 
 
-postsDiv.innerHTML += `
+let post=item.data();
 
-<div style="border:1px solid black; padding:10px; margin:10px;">
+let id=item.id;
 
-<p>${post.text}</p>
+
+
+feed.innerHTML += `
+
+
+<div class="post-card">
+
+
+<div class="user">
+
+👤 ${post.username}
 
 </div>
 
+
+
+<div class="post-text">
+
+${post.text}
+
+</div>
+
+
+
+<div class="actions">
+
+
+<button onclick="likePost('${id}')">
+
+❤️ ${post.likes || 0}
+
+</button>
+
+
+
+<button onclick="commentPost('${id}')">
+
+💬 ${post.comments || 0}
+
+</button>
+
+
+
+<button onclick="sharePost('${id}')">
+
+🔗 ${post.shares || 0}
+
+</button>
+
+
+
+<button onclick="repostPost('${id}')">
+
+🔁 ${post.reposts || 0}
+
+</button>
+
+
+</div>
+
+
+
+</div>
+
+
 `;
 
+
 });
 
 
 });
+
+
+
+
+// LIKE
+
+window.likePost = async function(id){
+
+await updateDoc(
+
+doc(db,"posts",id),
+
+{
+
+likes:increment(1)
+
+}
+
+);
+
+};
+
+
+
+
+// COMMENT COUNT
+
+window.commentPost = async function(id){
+
+await updateDoc(
+
+doc(db,"posts",id),
+
+{
+
+comments:increment(1)
+
+}
+
+);
+
+
+alert("Comment system will be connected next");
+
+};
+
+
+
+
+// SHARE
+
+window.sharePost = async function(id){
+
+await updateDoc(
+
+doc(db,"posts",id),
+
+{
+
+shares:increment(1)
+
+}
+
+);
+
+
+alert("Post shared");
+
+};
+
+
+
+
+// REPOST
+
+window.repostPost = async function(id){
+
+await updateDoc(
+
+doc(db,"posts",id),
+
+{
+
+reposts:increment(1)
+
+}
+
+);
+
+
+};
