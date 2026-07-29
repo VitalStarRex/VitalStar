3f048adc5b65e0f02bb3cf20d3dda45e0763d28e
VitalStarRex <amehchristianc@gmail.com>
2026-07-29 11:28:03 +0100

    Create users.js

---
+++ users.js
@@ -0,0 +1,40 @@
+import { db } from "./firebase.js";
+
+import {
+  collection,
+  query,
+  orderBy,
+  onSnapshot
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const usersList = document.getElementById("usersList");
+
+const q = query(
+  collection(db, "users"),
+  orderBy("username", "asc")
+);
+
+onSnapshot(q, (snapshot) => {
+
+  usersList.innerHTML = "";
+
+  snapshot.forEach((doc) => {
+
+    const user = doc.data();
+
+    usersList.innerHTML += `
+      <div class="user" onclick="location.href='chat.html?uid=${doc.id}'">
+        <div class="avatar">
+          ${user.username.charAt(0).toUpperCase()}
+        </div>
+
+        <div class="info">
+          <div class="name">${user.username}</div>
+          <div class="status">Tap to chat</div>
+        </div>
+      </div>
+    `;
+
+  });
+
+});
