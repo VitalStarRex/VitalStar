80adb929eadbf82b24d1f72e63db3cf1656d1c91
VitalStarRex <amehchristianc@gmail.com>
2026-07-29 17:09:26 +0100

    Update notifications.js

--- notifications.js
+++ notifications.js
@@ -1,40 +1,13 @@
-import { messaging, db } from "./firebase.js";
+import { messaging } from "./firebase.js";
 
-import {
-  getToken,
-  onMessage
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js";
+import { getToken, onMessage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js";
 
-import {
-  doc,
-  updateDoc
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-async function enableNotifications() {
-  const permission = await Notification.requestPermission();
-
-  if (permission !== "granted") {
-    alert("Notifications are disabled.");
-    return;
-  }
-
-  const token = await getToken(messaging, {
+const token = await getToken(messaging, {
     vapidKey: "BEkwoctvtqjDmybrhAY-gGrG8_aBxTBmxDUoqq5w43H8MW6z0IwvOzmCLI3AZKY1KLqc5YuTFrt2cL-952QjV7o"
-  });
+});
 
-  console.log("FCM Token:", token);
-
-  const uid = localStorage.getItem("uid");
-
-  if (uid && token) {
-    await updateDoc(doc(db, "users", uid), {
-      fcmToken: token
-    });
-  }
-}
-
-enableNotifications();
+console.log("VitalStar Notification Token:", token);
 
 onMessage(messaging, (payload) => {
-  alert(payload.notification.title + "\n\n" + payload.notification.body);
+    console.log("New notification:", payload);
 });
