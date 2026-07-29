62a71837e16d2a8bdceb180038bed06b4a1e026a
VitalStarRex <amehchristianc@gmail.com>
2026-07-29 17:31:15 +0100

    Update notifications.js

--- notifications.js
+++ notifications.js
@@ -1,18 +1,29 @@
 import { messaging } from "./firebase.js";
 import { getToken, onMessage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js";
 
-try {
+(async () => {
+  try {
+    const permission = await Notification.requestPermission();
+
+    if (permission !== "granted") {
+      alert("Notification permission was not granted.");
+      return;
+    }
+
     const token = await getToken(messaging, {
-        vapidKey: "BEkwoctvtqjDmybrhAY-gGrG8_aBxTBmxDUoqq5w43H8MW6z0IwvOzmCLI3AZKY1KLqc5YuTFrt2cL-952QjV7o"
+      vapidKey: "BEkwoctvtqjDmybrhAY-gGrG8_aBxTBmxDUoqq5w43H8MW6z0IwvOzmCLI3AZKY1KLqc5YuTFrt2cL-952QjV7o"
     });
 
-    console.log("VitalStar Notification Token:", token);
-} catch (error) {
-    console.error("Error getting notification token:", error);
-}
+    alert("Token: " + token);
+    console.log("Token:", token);
+
+  } catch (error) {
+    alert("Notification Error: " + error.message);
+    console.error(error);
+  }
+})();
 
 onMessage(messaging, (payload) => {
-    console.log("New notification:", payload);
+  alert("New notification received!");
+  console.log(payload);
 });
-
-alert("notification.js is running");
