bbceb540a603b894fbadc3e8772b8a199f45ba0a
VitalStarRex <amehchristianc@gmail.com>
2026-07-29 19:03:44 +0100

    Update create-post.js

--- create-post.js
+++ create-post.js
@@ -1,34 +1,209 @@
-import { db, auth } from "./firebase.js";
+import {db} from "./firebase.js";
+
 
 import {
- addDoc,
- collection,
- serverTimestamp
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+collection,
+query,
+orderBy,
+onSnapshot,
+doc,
+updateDoc,
+increment
 
-window.createPost = async function(){
-
-const text = document.getElementById("postText").value;
-
-const user = auth.currentUser;
-
-
-if(!user){
- alert("Please login first");
- return;
 }
 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-await addDoc(collection(db,"posts"),{
 
-uid:user.uid,
-text:text,
-createdAt:serverTimestamp()
+
+const feed = document.getElementById("feed");
+
+
+
+const q = query(
+
+collection(db,"posts"),
+
+orderBy("createdAt","desc")
+
+);
+
+
+
+onSnapshot(q,(snapshot)=>{
+
+
+feed.innerHTML="";
+
+
+snapshot.forEach((item)=>{
+
+
+let post=item.data();
+
+let id=item.id;
+
+
+
+feed.innerHTML += `
+
+
+<div class="post-card">
+
+
+<div class="user">
+
+👤 ${post.username}
+
+</div>
+
+
+
+<div class="post-text">
+
+${post.text}
+
+</div>
+
+
+
+<div class="actions">
+
+
+<button onclick="likePost('${id}')">
+
+❤️ ${post.likes || 0}
+
+</button>
+
+
+
+<button onclick="commentPost('${id}')">
+
+💬 ${post.comments || 0}
+
+</button>
+
+
+
+<button onclick="sharePost('${id}')">
+
+🔗 ${post.shares || 0}
+
+</button>
+
+
+
+<button onclick="repostPost('${id}')">
+
+🔁 ${post.reposts || 0}
+
+</button>
+
+
+</div>
+
+
+
+</div>
+
+
+`;
+
 
 });
 
 
-alert("Post created!");
+});
+
+
+
+
+// LIKE
+
+window.likePost = async function(id){
+
+await updateDoc(
+
+doc(db,"posts",id),
+
+{
+
+likes:increment(1)
+
+}
+
+);
+
+};
+
+
+
+
+// COMMENT COUNT
+
+window.commentPost = async function(id){
+
+await updateDoc(
+
+doc(db,"posts",id),
+
+{
+
+comments:increment(1)
+
+}
+
+);
+
+
+alert("Comment system will be connected next");
+
+};
+
+
+
+
+// SHARE
+
+window.sharePost = async function(id){
+
+await updateDoc(
+
+doc(db,"posts",id),
+
+{
+
+shares:increment(1)
+
+}
+
+);
+
+
+alert("Post shared");
+
+};
+
+
+
+
+// REPOST
+
+window.repostPost = async function(id){
+
+await updateDoc(
+
+doc(db,"posts",id),
+
+{
+
+reposts:increment(1)
+
+}
+
+);
+
 
 };
