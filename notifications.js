65348ebc60d8ca1290251f16096333f531bbc028
VitalStarRex <amehchristianc@gmail.com>
2026-07-29 17:12:26 +0100

    Update notifications.js

--- notifications.js
+++ notifications.js
@@ -1,12 +1,15 @@
 import { messaging } from "./firebase.js";
-
 import { getToken, onMessage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js";
 
-const token = await getToken(messaging, {
-    vapidKey: "BEkwoctvtqjDmybrhAY-gGrG8_aBxTBmxDUoqq5w43H8MW6z0IwvOzmCLI3AZKY1KLqc5YuTFrt2cL-952QjV7o"
-});
+try {
+    const token = await getToken(messaging, {
+        vapidKey: "BEkwoctvtqjDmybrhAY-gGrG8_aBxTBmxDUoqq5w43H8MW6z0IwvOzmCLI3AZKY1KLqc5YuTFrt2cL-952QjV7o"
+    });
 
-console.log("VitalStar Notification Token:", token);
+    console.log("VitalStar Notification Token:", token);
+} catch (error) {
+    console.error("Error getting notification token:", error);
+}
 
 onMessage(messaging, (payload) => {
     console.log("New notification:", payload);
