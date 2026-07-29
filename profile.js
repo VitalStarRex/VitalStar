8c57a75ad6bce462451b843e04d73549a17956fe
VitalStarRex <amehchristianc@gmail.com>
2026-07-29 12:49:56 +0100

    Update profile.js

--- profile.js
+++ profile.js
@@ -4,3 +4,9 @@ import {
   doc,
   getDoc
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const params = new URLSearchParams(window.location.search);
+const uid = params.get("uid");
+
+const userRef = doc(db, "users", uid);
+const userSnap = await getDoc(userRef);
