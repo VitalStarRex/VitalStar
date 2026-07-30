5071c7412e8b8565b2906db14381fea81739f713
VitalStarRex <amehchristianc@gmail.com>
2026-07-30 17:33:00 +0100

    Update home.js

--- home.js
+++ home.js
@@ -1,3 +1,39 @@
+
+
+
+
+import { auth } from "./firebase.js";
+
+import {
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+const logoutBtn = document.getElementById("logoutBtn");
+
+logoutBtn.addEventListener("click", async () => {
+  try {
+    await signOut(auth);
+
+    // Send user back to login page
+    window.location.href = "login.html";
+
+  } catch (error) {
+    alert(error.message);
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
 import { db, auth } from "./firebase.js";
 
 import {
