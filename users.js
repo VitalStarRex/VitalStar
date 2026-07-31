a8e6e355beb9f1e456f66194aa639209dae86bff
VitalStarRex <amehchristianc@gmail.com>
2026-07-31 08:20:22 +0100

    Update users.js

--- users.js
+++ users.js
@@ -1,52 +1,50 @@
+import { auth, db } from "./firebase.js";
 
-import { auth } from "./firebase.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+    collection,
+    query,
+    orderBy,
+    onSnapshot
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 onAuthStateChanged(auth, (user) => {
     if (!user) {
         window.location.href = "login.html";
+        return;
     }
 });
-
-
-
-
-
-import {
-  collection,
-  query,
-  orderBy,
-  onSnapshot
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const usersList = document.getElementById("usersList");
 
 const q = query(
-  collection(db, "users"),
-  orderBy("fullname", "asc")
+    collection(db, "users"),
+    orderBy("fullName", "asc")
 );
 
 onSnapshot(q, (snapshot) => {
 
-  usersList.innerHTML = "";
+    usersList.innerHTML = "";
 
-  snapshot.forEach((doc) => {
+    snapshot.forEach((doc) => {
 
-    const user = doc.data();
+        const user = doc.data();
+        const name = user.fullName || "User";
 
-    usersList.innerHTML += `
-      <div class="user" onclick="location.href='profile.html?uid=${doc.id}'">
-        <div class="avatar">
-          ${user.fullname.charAt(0).toUpperCase()}
-        </div>
+        usersList.innerHTML += `
+            <div class="user" onclick="location.href='profile.html?uid=${doc.id}'">
+                <div class="avatar">
+                    ${name.charAt(0).toUpperCase()}
+                </div>
 
-        <div class="info">
-          <div class="name">${user.fullname}</div>
-          <div class="status">Tap to chat</div>
-        </div>
-      </div>
-    `;
+                <div class="info">
+                    <div class="name">${name}</div>
+                    <div class="status">Tap to chat</div>
+                </div>
+            </div>
+        `;
 
-  });
+    });
 
 });
