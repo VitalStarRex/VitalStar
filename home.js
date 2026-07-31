7b20a0264497b1812ee5a08b5fd590322679bb91
VitalStarRex <amehchristianc@gmail.com>
2026-07-31 07:35:25 +0100

    Update home.js

--- home.js
+++ home.js
@@ -1,13 +1,8 @@
-import { auth } from "./firebase.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { auth, db } from "./firebase.js";
 
-onAuthStateChanged(auth, (user) => {
-    if (!user) {
-        window.location.href = "login.html";
-    }
-});
-
-
+import {
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 import {
     collection,
