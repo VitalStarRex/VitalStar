096fe3b99cebc9c6bba5822ca1667d61e3dbe34a
VitalStarRex <amehchristianc@gmail.com>
2026-08-01 21:29:20 +0100

    Update comments.js

--- comments.js
+++ comments.js
@@ -1,5 +1,9 @@
 
-import { auth } from "./firebase.js";
+import { auth, db } from "./firebase.js";
+
+
+
+
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 onAuthStateChanged(auth, (user) => {
