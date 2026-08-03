40fb71ebe0312d23f193d591887af5117fea4e58
VitalStarRex <amehchristianc@gmail.com>
2026-08-03 14:51:56 +0100

    Update notification-badge.js

--- notification-badge.js
+++ notification-badge.js
@@ -3,7 +3,7 @@ import { auth, db } from "./firebase.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 import {
-    collection,
+    collectionGroup,
     query,
     where,
     onSnapshot
