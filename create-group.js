ca7150b28f7e3437a392e0779d8638b7a89df06f
VitalStarRex <amehchristianc@gmail.com>
2026-08-07 17:13:36 +0100

    Update create-group.js

--- create-group.js
+++ create-group.js
@@ -5,7 +5,7 @@
 // the group document (+ owner membership) in Firestore.
 // ============================================================
 
-import { auth, db } from '../firebase-config.js';
+import { auth, db } from '../firebase.js';
 import {
   onAuthStateChanged
 } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js';
