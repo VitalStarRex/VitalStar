80ee7d1d6b4253e9658fb5c6b305476bd7f36361
VitalStarRex <amehchristianc@gmail.com>
2026-09-21 19:33:46 +0100

    Update home.js

--- home.js
+++ home.js
@@ -29,7 +29,28 @@ import {
 import {
     ref,
     onValue
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js
+
+
+
+
+
+
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        console.log('Service worker registered with scope:', registration.scope);
+      })
+      .catch(error => {
+        console.log('Service worker registration failed:', error);
+      });
+  });
+}
+
+
 
 
 // ============================================================
